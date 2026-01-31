5da2777897e81d4375d9709111ad012d0cd0a319
martinaspeciale <mspeciale127001@gmail.com>
2026-01-31 20:36:12 +0100

    model: added mistral llm option

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
+from langchain_mistralai import ChatMistralAI
 
 load_dotenv()
 
@@ -10,4 +11,10 @@ llm = ChatGroq(
     temperature=0, 
     model_name="llama-3.3-70b-versatile",
     api_key=os.environ.get("GROQ_API_KEY")
+)
+
+llm = ChatMistralAI(
+    model="mistral-large-latest",  # Consigliato per logica complessa (Router/Planner)
+    # model="open-mixtral-8x22b",  # Alternativa open source molto forte
+    temperature=0
 )
